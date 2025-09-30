e33e09a9a103033b59cd8df3d0b4d860b0d0d9c5
구민서 <minseo12379@gmail.com>
2025-09-30 16:46:32 +0900

    refactor: 스크립트 디렉토리 구조 정리 및 경로 수정

--- hackathon-godot/scripts/Customer.gd
+++
@@ -1,5 +0,0 @@
-# Customer.gd
-extends Node2D
-
-# 이 손님이 원하는 포션의 이름이 저장될 변수
-var wanted_potion: String = ""
---
+++ hackathon-godot/scripts/entities/Customer.gd
@@ -0,0 +1,5 @@
+# Customer.gd
+extends Node2D
+
+# 이 손님이 원하는 포션의 이름이 저장될 변수
+var wanted_potion: String = ""
